77618cc3ee26018a9a1c0f1b72b8adb8407a2900
Okan Mert TAN <okanmerttan@gmail.com>
2022-07-01 23:51:20 +0300

    JAVA

---
+++ Java/KarneOrtalaması.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Program 
+{
+    public static void main(String[] args) {
+        System.out.println("Karne Ortalaması Hesaplaması");
+        
+        Scanner input = new Scanner(System.in);
+        int matematikNotu , türkçeNotu , fizikNotu , kimyaNotu , karneOrt ;
+
+        System.out.println("Matematik Notunuz : ");
+        matematikNotu = input.nextInt();
+
+        System.out.println("Türkçe Notunuz : ");
+        türkçeNotu = input.nextInt();
+
+        System.out.println("Fizik Notunuz : ");
+        fizikNotu = input.nextInt();
+
+        System.out.println("Kimya Notunuz : ");
+        kimyaNotu = input.nextInt();
+
+        karneOrt = (matematikNotu+türkçeNotu+fizikNotu+kimyaNotu)/4;
+
+        System.out.println("Karne Ortalaması : "+ karneOrt);
+    }
+}
